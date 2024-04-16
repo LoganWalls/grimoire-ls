fabdeb63550aedcaa565ed5374e365d6037262a7
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2024-04-15 21:11:31 -0400

    poetry -> uv

--- flake.nix
+++ flake.nix
@@ -16,24 +16,25 @@
         devShells.${system}.default =
           pkgs.mkShell
           {
-            packages = [
-              pkgs.python310
-              pkgs.poetry
+            packages = with pkgs; [
+              python310
+              uv
             ];
 
             LD_LIBRARY_PATH = lib.optional stdenv.isLinux (pkgs.lib.makeLibraryPath [
               pkgs.stdenv.cc.cc
             ]);
 
-            # Put the venv on the repo, so direnv can access it
-            POETRY_VIRTUALENVS_IN_PROJECT = "true";
-            POETRY_VIRTUALENVS_PATH = "{project-dir}/.venv";
-
-            # Use python from path, so you can use a different version to the one bundled with poetry
-            POETRY_VIRTUALENVS_PREFER_ACTIVE_PYTHON = "true";
-
             # Use ipdb
             PYTHONBREAKPOINT = "ipdb.set_trace";
+
+            shellHook = ''
+              if [ ! -d .venv ]; then
+                uv venv
+                uv pip install -e .
+              fi
+              source .venv/bin/activate
+            '';
           };
       }
     );
