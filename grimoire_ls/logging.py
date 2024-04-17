8ee5a4a3da9799d4d64041b089d1f3de440c9790
Logan Walls <2934282+LoganWalls@users.noreply.github.com>
2024-04-16 21:57:33 -0400

    fix logging path

--- grimoire_ls/logging.py
+++ grimoire_ls/logging.py
@@ -1,10 +1,16 @@
+import os
 from pathlib import Path
 
-path = Path("~/Projects/grimoire_ls/grimoire_ls.log").expanduser()
-path.write_text("")
+path = Path(
+    os.environ.get(
+        "GRIMOIRE_LS_LOG", Path(".").expanduser().resolve() / "grimoire-ls.log"
+    )
+)
 
 
 def log(x):
+    """Log an object to the log file specified by the `GRIMOIRE_LS_LOG` environment variable.
+    If the variable is not set, the log file will be created in the current working directory."""
     if not isinstance(x, str):
         x = repr(x)
     with path.open("a") as f:
